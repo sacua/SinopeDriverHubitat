0bac4df92bfadfc6695ca595ee577cf8b1ae7a0e
sacua <59889660+sacua@users.noreply.github.com>
2022-05-06 20:31:04 -0400

    Update TH112xZB_Sinope_Hubitat.groovy

--- drivers/TH112xZB_Sinope_Hubitat.groovy
+++ drivers/TH112xZB_Sinope_Hubitat.groovy
@@ -15,7 +15,10 @@
  *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
- *
+ * 
+ * v1.0.0 Initial commit
+ * v1.1.0 Dont remember
+ * v1.2.0 Correction for attribute reading for heat and off command
  */
 
 metadata {
