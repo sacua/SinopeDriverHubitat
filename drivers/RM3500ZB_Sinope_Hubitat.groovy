ee1b6c21847efc499a394643ae4ba74ffe7212f5
PJ <120332908+iamtrep@users.noreply.github.com>
2023-02-13 21:11:16 -0500

    camelcase coherency for powerReport preference

--- drivers/RM3500ZB_Sinope_Hubitat.groovy
+++ drivers/RM3500ZB_Sinope_Hubitat.groovy
@@ -48,7 +48,7 @@ metadata
 
     preferences {
         input name: "tempChange", type: "number", title: "Temperature change", description: "Minumum change of temperature reading to trigger report in Celsius/100, 10..200", range: "10..200", defaultValue: 100
-        input name: "PowerReport", type: "number", title: "Power change", description: "Amount of wattage difference to trigger power report (1..*)",  range: "1..*", defaultValue: 30
+        input name: "powerReport", type: "number", title: "Power change", description: "Amount of wattage difference to trigger power report (1..*)",  range: "1..*", defaultValue: 30
         input name: "energyChange", type: "number", title: "Energy increment", description: "Minimum increment of the energy meter in Wh to trigger energy reporting (10..*)", range: "10..*", defaultValue: 10
         input name: "energyPrice", type: "float", title: "c/kWh Cost:", description: "Electric Cost per Kwh in cent", range: "0..*", defaultValue: 9.38
         input name: "weeklyReset", type: "enum", title: "Weekly reset day", description: "Day on which the weekly energy meter return to 0", options:["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"], defaultValue: "Sunday", multiple: false, required: true
@@ -233,15 +233,15 @@ def configure(){
     // Configure Reporting
     if (tempChange == null)
 		tempChange = 100 as int
-    if (PowerReport == null)
-		PowerReport = 30 as int
+    if (powerReport == null)
+		powerReport = 30 as int
 	if (energyChange == null)
 		energyChange = 10 as int
 
     cmds += zigbee.configureReporting(0x0402, 0x0000, 0x29, 30, 580, (int) tempChange)  //local temperature
     cmds += zigbee.configureReporting(0x0500, 0x0002, DataType.BITMAP16, 0, 600, null)
     cmds += zigbee.configureReporting(0x0006, 0x0000, 0x10, 0, 600, null)           //On off state
-    cmds += zigbee.configureReporting(0x0B04, 0x050B, 0x29, 30, 600, (int) PowerReport)
+    cmds += zigbee.configureReporting(0x0B04, 0x050B, 0x29, 30, 600, (int) powerReport)
     cmds += zigbee.configureReporting(0x0702, 0x0000, DataType.UINT48, 299, 1799, (int) energyChange) //Energy reading
 
 	// Configure Water Temp Min
